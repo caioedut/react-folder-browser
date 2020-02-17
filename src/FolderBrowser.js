13cae3d3c77be5d4eb4d86f9b024a7b97ef78986
Caio Teixeira <caioedut@gmail.com>
2020-02-17 00:44:50 -0300

    File drop and propTypes

--- src/FolderBrowser.js
+++ src/FolderBrowser.js
@@ -1,11 +1,74 @@
 import './styles.css';
 
 import React, {useEffect, useState} from 'react';
+import PropTypes from 'prop-types';
+
+import FileDrop from 'react-file-drop';
+
+export const propTypes = {
+  title: PropTypes.node,
+  loading: PropTypes.node,
+  deleting: PropTypes.node,
+  uploading: PropTypes.node,
+  error: PropTypes.node,
+  empty: PropTypes.node,
+  drop: PropTypes.node,
+  exists: PropTypes.string,
+  delete: PropTypes.string,
+  columnName: PropTypes.node,
+  columnSize: PropTypes.node,
+  columnPreview: PropTypes.node,
+  columnActions: PropTypes.node,
+  ignoreDuplicated: PropTypes.bool,
+  buttonRefresh: PropTypes.shape({
+    title: PropTypes.node,
+    className: PropTypes.string,
+    children: PropTypes.oneOfType([
+      PropTypes.node,
+      PropTypes.arrayOf(PropTypes.node),
+    ]),
+    onClick: PropTypes.func.isRequired,
+  }),
+  buttonUpload: PropTypes.shape({
+    title: PropTypes.node,
+    className: PropTypes.string,
+    children: PropTypes.oneOfType([
+      PropTypes.node,
+      PropTypes.arrayOf(PropTypes.node),
+    ]),
+    onChange: PropTypes.func.isRequired,
+  }),
+  buttonDownload: PropTypes.shape({
+    title: PropTypes.node,
+    className: PropTypes.string,
+    children: PropTypes.oneOfType([
+      PropTypes.node,
+      PropTypes.arrayOf(PropTypes.node),
+    ]),
+    download: PropTypes.oneOfType([
+      PropTypes.bool,
+      PropTypes.string,
+    ]),
+  }),
+  buttonDelete: PropTypes.shape({
+    title: PropTypes.node,
+    className: PropTypes.string,
+    children: PropTypes.oneOfType([
+      PropTypes.node,
+      PropTypes.arrayOf(PropTypes.node),
+    ]),
+    onClick: PropTypes.func.isRequired,
+  }),
+};
 
 export const defaultProps = {
   title: 'Files',
   loading: 'Loading...',
+  deleting: 'Deleting...',
+  uploading: 'Uploading...',
+  error: 'Error',
   empty: 'No files.',
+  drop: 'Drop files here',
   exists: 'File already exists. Continue?',
   delete: 'Do you want to remove the file?',
   columnName: 'Name',
@@ -45,6 +108,7 @@ function formatBytes(bytes, decimals = 2) {
 
 export default function FolderBrowser(props) {
   const [files, setFiles] = useState(null);
+  const [modalFileUrl, setModalFileUrl] = useState(null);
 
   useEffect(() => setFiles(props.files), [props.files]);
 
@@ -52,6 +116,34 @@ export default function FolderBrowser(props) {
   const buttonUpload = {...defaultProps.buttonUpload, ...props.buttonUpload};
   const buttonDownload = {...defaultProps.buttonDownload, ...props.buttonDownload};
   const buttonDelete = {...defaultProps.buttonDelete, ...props.buttonDelete};
+
+  const {additionalColumns} = props;
+
+  function _handleDrop(files, e) {
+    _handleChange({target: {files}}, files);
+  }
+
+  function _handleChange(e) {
+    const files = e.target.files;
+
+    if (files) {
+      let exists = false;
+
+      // Check if exists
+      for (const file of Array.from(files)) {
+        if (data.find(({name}) => file.name === name)) {
+          exists = true;
+          break;
+        }
+      }
+
+      if (!exists || window.confirm(props.exists)) {
+        buttonUpload.onChange(e, files);
+      }
+
+      e.target.value = null;
+    }
+  }
 
   function _handleFocus(e, file) {
     const $el = e.currentTarget;
@@ -71,132 +163,161 @@ export default function FolderBrowser(props) {
   );
 
   return (
-    <div id="folder-browser">
-      <div id="folder-browser-header">
-        <div id="folder-browser-title">
-          {props.title}
+    <div className="folder-browser">
+      {modalFileUrl && (
+        <div className="folder-browser-modal" onClick={setModalFileUrl.bind(null, null)}>
+          <img src={modalFileUrl} alt="" onClick={(e) => e.stopPropagation()}/>
+          <div onClick={setModalFileUrl.bind(null, null)}>×</div>
         </div>
-        <div id="folder-browser-buttons">
-          <button
-            className="folder-browser-button"
-            type="button"
-            children={buttonRefresh.title}
-            {...buttonRefresh}
-          />
-          <label className="folder-browser-button" {...buttonUpload} onChange={null}>
-            {buttonUpload.children || buttonUpload.title}
-            <input
-              type="file"
-              name="file"
-              hidden
-              onChange={(e) => {
-                const files = e.target.files;
-                const exists = data.find(({name}) => name === files[0].name);
-
-                if (!exists || window.confirm(props.exists)) {
-                  buttonUpload.onChange(e, e.target.files);
-                }
-
-                e.target.value = null;
-              }}
+      )}
+      <FileDrop onDrop={_handleDrop}>
+        <div className="folder-browser-dragbanner">
+          {props.drop}
+        </div>
+        <div className="folder-browser-header">
+          <div className="folder-browser-title">
+            {props.title}
+          </div>
+          <div className="folder-browser-buttons">
+            <button
+              className="folder-browser-button"
+              type="button"
+              children={buttonRefresh.title}
+              {...buttonRefresh}
             />
-          </label>
+            <label className="folder-browser-button" {...buttonUpload} onChange={null}>
+              {buttonUpload.children || buttonUpload.title}
+              <input
+                className="folder-browser-input"
+                type="file"
+                name="file"
+                multiple
+                hidden
+                onChange={_handleChange}
+              />
+            </label>
+          </div>
         </div>
-      </div>
-      <div id="folder-browser-body">
+        <div className="folder-browser-body">
 
-        {!data ? (
-          <div style={{padding: '0 15px 15px'}}>{props.loading}</div>
-        ) : !data.length ? (
-          <div style={{padding: '0 15px 15px'}}>{props.empty}</div>
-        ) : (
-          <table>
-            <thead>
-            <tr>
-              <th>{props.columnPreview}</th>
-              <th>{props.columnName}</th>
-              <th/>
-              <th>{props.columnSize}</th>
-              <th>{props.columnActions}</th>
-            </tr>
-            </thead>
-            <tbody>
+          {!data ? (
+            <div style={{padding: '0 15px 15px'}}>{props.loading}</div>
+          ) : !data.length ? (
+            <div style={{padding: '0 15px 15px'}}>{props.empty}</div>
+          ) : (
+            <table>
+              <thead>
+              <tr>
+                <th>{props.columnPreview}</th>
+                <th>{props.columnName}</th>
+                <th/>
 
-            {data.map((file, i) => (
-              <tr key={i} onClick={(e) => _handleFocus(e, file)}>
-                <td>
-                  {[
-                    'gif', 'ico', 'jpeg', 'jpg', 'png', 'svg', 'tif', 'tiff', 'webp'
-                  ].some((ext) => {
-                    return file.name.toLowerCase().endsWith(ext.toLowerCase());
-                  }) && (
-                    <div style={{
-                      position: 'relative',
-                      backgroundColor: '#eee',
-                      overflow: 'hidden',
-                      height: 38,
-                      width: 38,
-                    }}>
-                      <img
-                        src={file.url}
-                        alt={file.name}
-                        style={{
-                          objectFit: 'cover',
-                          height: '100%',
-                          width: '100%',
+                {additionalColumns && additionalColumns.map((column, key) => (
+                  <th key={key}>{column.header}</th>
+                ))}
+
+                <th>{props.columnSize}</th>
+                <th>{props.columnActions}</th>
+              </tr>
+              </thead>
+              <tbody>
+
+              {data.map((file, i) => {
+                const isImage = [
+                  'gif', 'ico', 'jpeg', 'jpg', 'png', 'svg', 'tif', 'tiff', 'webp'
+                ].some((ext) => {
+                  return file.name.toLowerCase().endsWith(ext.toLowerCase());
+                });
+
+                return (
+                  <tr key={i} onClick={(e) => _handleFocus(e, file)}>
+                    <td>
+                      {isImage && (
+                        <div style={{
+                          position: 'relative',
+                          backgroundColor: '#eee',
+                          overflow: 'hidden',
+                          height: 38,
+                          width: 38,
+                        }}>
+                          <img
+                            src={file.url}
+                            alt={file.name}
+                            style={{
+                              cursor: 'pointer',
+                              objectFit: 'cover',
+                              height: '100%',
+                              width: '100%',
+                            }}
+                            onClick={setModalFileUrl.bind(null, file.url)}
+                          />
+                        </div>
+                      )}
+                    </td>
+                    <td style={{width: '100%'}}>
+                      <a href={file.url} target={'_blank'} onClick={(e) => {
+                        if (isImage) {
+                          e.preventDefault();
+                          setModalFileUrl(file.url);
+                        }
+                      }}>
+                        {file.name}
+                      </a>
+                    </td>
+                    <td className="folder-browser-nowrap">
+                      {file.uploading && (
+                        <b>{props.uploading}</b>
+                      )}
+                      {file.deleting && (
+                        <b>{props.deleting}</b>
+                      )}
+                      {file.error && (
+                        <div>
+                          <b>{props.error}</b>
+                        </div>
+                      )}
+                    </td>
+
+                    {additionalColumns && additionalColumns.map((column, key) => (
+                      <th key={key}>{column.render(file)}</th>
+                    ))}
+
+                    <td className="folder-browser-nowrap" align="right">
+                      {formatBytes(file.size)}
+                    </td>
+                    <td className="folder-browser-nowrap">
+                      <a
+                        className="folder-browser-button"
+                        children={buttonDownload.title}
+                        {...buttonDownload}
+                        href={file.url}
+                      />
+                      <button
+                        className="folder-browser-button"
+                        type="button"
+                        children={buttonDelete.title}
+                        {...buttonDelete}
+                        onClick={(e) => {
+                          if (window.confirm(props.delete)) {
+                            buttonDelete.onClick(e, file);
+                          }
                         }}
                       />
-                    </div>
-                  )}
-                </td>
-                <td style={{width: '100%'}}>
-                  <a href={file.url} target={'_blank'}>
-                    {file.name}
-                  </a>
-                </td>
-                <td className="folder-browser-nowrap">
-                  {file.loading && (
-                    <b>Enviando...</b>
-                  )}
-                  {file.error && (
-                    <div>
-                      <b>Falha no envio</b>
-                    </div>
-                  )}
-                </td>
-                <td className="folder-browser-nowrap">
-                  {formatBytes(file.size)}
-                </td>
-                <td className="folder-browser-nowrap">
-                  <a
-                    className="folder-browser-button"
-                    children={buttonDownload.title}
-                    {...buttonDownload}
-                    href={file.url}
-                  />
-                  <button
-                    className="folder-browser-button"
-                    type="button"
-                    children={buttonDelete.title}
-                    {...buttonDelete}
-                    onClick={(e) => {
-                      if (window.confirm(props.delete)) {
-                        buttonDelete.onClick(e, file);
-                      }
-                    }}
-                  />
-                </td>
-              </tr>
-            ))}
+                    </td>
+                  </tr>
+                );
+              })}
 
-            </tbody>
-          </table>
-        )}
+              </tbody>
+            </table>
+          )}
 
-      </div>
+        </div>
+      </FileDrop>
     </div>
   );
 
 };
 
+FolderBrowser.propTypes = propTypes;
 FolderBrowser.defaultProps = defaultProps;
